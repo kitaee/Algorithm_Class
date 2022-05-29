3d5f2f8c3e1aba64619b7ec5e866e6d351796ca4
이기태 <91439717+kitaee@users.noreply.github.com>
2022-05-29 21:26:29 +0900

    Update 3.c

--- Assignment #4/3.c
+++ Assignment #4/3.c
@@ -3,6 +3,7 @@
 typedef struct ITEM{
     int weight;
     int value;
+    int originalIndex;
 }item;
 
 item itemArr[6];
@@ -16,16 +17,22 @@ int main(void){
 
     itemArr[0].weight = 6;
     itemArr[0].value = 60;
+    itemArr[0].originalIndex = 1;
     itemArr[1].weight = 10;
     itemArr[1].value = 20;
+    itemArr[1].originalIndex = 2;
     itemArr[2].weight = 3;
     itemArr[2].value = 12;
+    itemArr[2].originalIndex = 3;
     itemArr[3].weight = 5;
     itemArr[3].value = 80;
+    itemArr[3].originalIndex = 4;
     itemArr[4].weight = 1;           
     itemArr[4].value = 30;
+    itemArr[4].originalIndex = 5;
     itemArr[5].weight = 3;
     itemArr[5].value = 60;
+    itemArr[5].originalIndex = 6;
 
     sortByValueDivWeight();
     
@@ -50,19 +57,21 @@ void sortByValueDivWeight(){
 }
 int greedyKnapsack(){
     int capacity = 16;
+    int currentWeight = 0;
     int maximumValue = 0;
     int stopIndex;
     int lastFraction;
     for(int i=0;i<6;i++){
         maximumValue += itemArr[i].value;
-        capacity -= itemArr[i].weight;
-        if(capacity-itemArr[i+1].weight<0){
+        currentWeight += itemArr[i].weight;
+        printf("Item Index : %d, Fraction Number = 1\n",itemArr[i].originalIndex);
+        if(currentWeight + itemArr[i+1].weight>16){
             stopIndex = i+1;
             break;
         }
-        printf("Item Indices")
     }
     lastFraction = itemArr[stopIndex].value / itemArr[stopIndex].weight;
-    maximumValue += (lastFraction*capacity);
+    maximumValue += (lastFraction*(capacity-currentWeight));
+    printf("Item Index : %d, Fraction Nubmer = %f\n",itemArr[stopIndex].originalIndex,(double)1/3);
     return maximumValue;
 }
